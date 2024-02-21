4643207f980326cef251b5cd7aa1468a59a5366f
spark-02 <>
2024-02-21 15:58:48 +0900

    テーブルビュー変更前

--- DogViewApp/DogViewApp/ViewController/DogListViewController.swift
+++ DogViewApp/DogViewApp/ViewController/DogListViewController.swift
@@ -11,8 +11,7 @@ class DogListViewController: UIViewController,UITableViewDelegate, UITableViewDa
     
     var dogResponce: dogResponce?
     let dog = Dog()
-    var breeds = [String]()
-  //  var dogs: [String] = []
+    var dogs: [String] = []
     var selectedDogName: String?
     
     @IBOutlet weak var DogList: UITableView!
@@ -30,9 +29,9 @@ class DogListViewController: UIViewController,UITableViewDelegate, UITableViewDa
         Task{
             let result = await dog.setDogList()
             switch result {
-            case .success(let dogBreeds):
-                self.dogResponce = dogBreeds
-                self.breeds = dogResponce?.message.keys.sorted() ?? []
+            case .success(let response):
+                self.dogResponce = response
+                self.dogs = response.message.keys.sorted()
                 self.DogList.reloadData()
             case .failure(let error):
                 print("Error: \(error.localizedDescription)")
@@ -41,18 +40,17 @@ class DogListViewController: UIViewController,UITableViewDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return breeds.count
+        return dogResponce?.message.count ?? 0//ここのコードを確認
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
-        // cell.textLabel?.text = dogs[indexPath.row]
-        let doglist =
+        cell.textLabel?.text = dogs[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       selectedDogName = dogs[indexPath.row]
+        selectedDogName = dogs[indexPath.row]
         performSegue(withIdentifier: "toCollection", sender: nil)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
